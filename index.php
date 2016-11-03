e6e1601f8404ddd1e5ef4158f7e3c5e2ff285382
Gustavo Ceccon <gustavofceccon@gmail.com>
2016-11-03 15:35:42 -0200

    Simple php page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CUDA - Recurso Educacional Aberto</title>
+</head>
+<body>
+  <span> HELLO WORLD </span>
+  <?php echo 'Ola mundo'; ?>
+</body>
+</html>
