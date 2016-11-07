85221120016791a9c6804155e9ea60793b7f9fb4
kori4r2 <xitsilvax@hotmail.com>
2016-11-06 23:54:53 -0200

    Code section finished and a sentence adjusted in introduction section
    The website is finished, let's go play some dota

--- assets/js/content/code-content.js
+++ assets/js/content/code-content.js
@@ -1,24 +1,60 @@
 var codeDescription = [
   {
     lines: [],
-    description: "<h1>Introduction to Code 1<\/h1> <br> \
-    <p class=\"description\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tortor nibh, iaculis sed elementum a, mattis ac nunc. Nunc placerat tristique tellus, ut accumsan erat mollis ut. Suspendisse ac quam pellentesque, dictum nisl eget, euismod felis. Donec ac rutrum mi. Suspendisse sit amet lacus consectetur, sagittis nisl ut, luctus est. Vivamus feugiat hendrerit nisl ut suscipit. Cras sed odio quis augue viverra accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ligula tellus, tincidunt nec quam ut, efficitur ullamcorper nulla.<\/p>"
+    description: "<h1>Code explanation<\/h1> <br> \
+    <p class=\"description\">&nbsp;&nbsp;&nbsp;In this section the portions of the code highlighted in the other tab will be explained. Do keep in mind these explanations will be focused on the specifities of the CUDA API, and pieces of code considered common for C/C++ languages will be ignored.<br><br>\
+    Note: If you have any doubts about any of the concepts mentioned here, please refer to the intro section.<\/p>"
   },
   {
     lines: [1, 2],
-    description: "<h1>Introduction to Code 2<\/h1> <br> \
-    <p class=\"description\">Phasellus semper odio et enim elementum, a semper neque accumsan. Aenean condimentum odio quis diam mollis, sit amet lobortis nisi auctor. Nam convallis erat sed dui hendrerit, ut dapibus eros convallis. Morbi sodales fermentum ante eu efficitur. Morbi iaculis urna arcu, vestibulum dictum erat hendrerit id. Pellentesque in purus lacus. Donec mollis efficitur commodo.<\/p>"
-  },
-  {
-    lines: [17],
-    description: "<h1>Introduction to Code 3<\/h1> <br> \
-    <p class=\"description\">Suspendisse potenti. Suspendisse scelerisque fringilla molestie. Sed lobortis ex at venenatis maximus. In congue dui vel congue suscipit. Nunc ac nunc risus. Pellentesque efficitur lorem bibendum, tincidunt augue condimentum, pharetra nibh. Quisque faucibus lectus tincidunt lobortis maximus. Suspendisse maximus volutpat dui at suscipit. Nulla efficitur lobortis nulla a feugiat. Phasellus nec sem lectus. Donec auctor nulla quis condimentum aliquam. Curabitur eu facilisis velit, sed mollis libero. Cras dapibus sapien porta elit dignissim tincidunt. Phasellus quis augue at velit vulputate volutpat in vitae tortor.<\/p>"
+    description: "<h1>Defines<\/h1> <br> \
+    <p class=\"description\">&nbsp;&nbsp;&nbsp;The first define, N, will be used to determine the size of the arrays created further ahead. In this case the value used is 2^20.<br>\
+    &nbsp;&nbsp;&nbsp;The other define, M, will be used to determine the size of the blocks created when making kernel launches with multiples threads.<\/p>"
   },
   {
     lines: [3, 12],
-    description: "<h1>Introduction to Code 4<\/h1> <br> \
-    <p class=\"description\">Curabitur nisi augue, blandit vitae risus non, vulputate blandit lacus. Curabitur maximus sem ac erat luctus consequat. Aliquam varius ullamcorper tellus non cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla gravida erat ligula, vehicula consequat ex aliquet pellentesque. Nulla vitae tortor luctus, vestibulum metus sit amet, posuere nulla. Nulla neque nibh, consectetur ut rhoncus at, suscipit sit amet urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse pharetra turpis et dolor interdum, ut luctus arcu tempus. In tincidunt felis quis risus posuere, sed mattis enim commodo. Fusce non turpis ante. Phasellus ac dolor quam. Duis posuere efficitur lectus ut dignissim. Sed euismod maximus augue, vel pulvinar libero scelerisque convallis. Integer ut tellus eget sem scelerisque vulputate. Vivamus efficitur mattis felis id efficitur.<\/p>"
-  }
+    description: "<h1>Device code: functions<\/h1> <br> \
+    <p class=\"description\">&nbsp;&nbsp;&nbsp;These are the functions that when called in the host code will be executed by the device. They will be explained in detail further ahead.<\/p>"
+  },
+  {
+    lines: [17],
+    description: "<h1>Host code: main function<\/h1> <br> \
+    <p class=\"description\">&nbsp;&nbsp;&nbsp;In this specific example the host code consists of only the main function, but other ones could easily be created if needed.<\/p>"
+  },
+  {
+      lines: [18, 19, 20, 21, 22, 23],
+      description: "<h1>Arrays<\/h1> <br> \
+      <p class=\"description\">&nbsp;&nbsp;&nbsp;The arrays used in the code are declared(note how the arrays that will be allocated inside the graphic memory are declared normally) and allocated here.<br> \
+      &nbsp;&nbsp;&nbsp;It's important to note that the cudaMalloc function, unlike it's counterpart, malloc, does not return the address of the allocated memory. and instead must receive the pointer's address as a parameter.<\/p>"
+  },
+  {
+      lines: [25, 26, 27, 28, 29, 30, 31],
+      description: "<h1>Filling up the arrays<\/h1> <br> \
+      <p class=\"description\">&nbsp;&nbsp;&nbsp;The loop first fills the host memory arrays, x and y, with <span class=\"code-variable\">1.0<\/span> and <span class=\"code-variable\">2.0<\/span> respectively.<br> \
+      &nbsp;&nbsp;&nbsp;The values are then copied to their device memory counterparts, d_x and d_y, using the cudaMemcpy function. Since the copy is from host memory to device memory, the appropriate parameter must be passed to the function.<\/p>"
+  },
+  {
+      lines: [33, 34, 12, 13, 14, 15],
+      description: "<h1>saxpy function<\/h1> <br> \
+      <p class=\"description\">&nbsp;&nbsp;&nbsp;The saxpy function creates many blocks and threads and makes it so every thread acesses a different index of the arrays, calculates (ax + y) and the saves the result to the corresponding position of the y array, all in parallel.<br> \
+      &nbsp;&nbsp;&nbsp;The first important thing to note here is the kernel launch. The second parameter's value is the defined M value, no surprises there, but the first parameter must be adjusted so that no matter the size of the array passed, all its members bust be processed by a different thread, even if the number of threads surpasses the necessary.<br> \
+      &nbsp;&nbsp;&nbsp;Inside the function itself, the i variable receives the index value of the numbers that will be processed by this specific thread, and then checks to see if the value is within bounds before calculating (ax + y) and saving the result in the y array. The boundary check is very important, because, as was just stated, the number of threads might be higher than the number of elements inside the array.<br> \
+      &nbsp;&nbsp;&nbsp;Back in the host code, there is a cudaMemcpy call that copies the contents of <span class=\"code-variable\">d_y<\/span> back to the host memory pointed by <span class=\"code-variable\">y<\/span>. Due to the nature of the cudaMemcpy funtion, we have a guarantee that the array copied will contain the results after the execution of saxpy is finished. Also note that, as this time the copy is made from device to memory, the last parameter is different.<\/p>"
+  },
+  {
+      lines: [35, 36, 3, 4, 5, 6, 7, 8, 9, 10],
+      description: "<h1>reverse function<\/h1> <br> \
+      <p class=\"description\">&nbsp;&nbsp;&nbsp;The reverse function, unlike the saxpy function, creates a single block and one thread for each array position and uses a shared memory between those threads to invert the contents of the array passed.<br> \
+      &nbsp;&nbsp;&nbsp;The kernel launch and the cudaMemcpy function inside the host code do not offer anything new for us to comment<br> \
+      &nbsp;&nbsp;&nbsp;Inside the function though, there is a shared variable created. This array has the same size as the one to be reversed and, because of the __shared__ keyword, is created only once, since there is only one block.<br> \
+      &nbsp;&nbsp;&nbsp;The <span class=\"code-variable\">t<\/span> variable contains the current position of the array, and the <span class=\"code-variable\">t<\/span> variable contains it's mirrored position inside the array.<br> \
+      &nbsp;&nbsp;&nbsp;The values of the original array are first copied to the auxiliar variable independently by each thread. There is then a call to the __syncthreads function, to guarantee that the values have been copied before they are copied back to the original array in their mirrored positions.<\/p>"
+  },
+  {
+      lines: [39, 40, 41],
+      description: "<h1>Memory deallocation and end of main function<\/h1> <br> \
+      <p class=\"description\">After the execution is finished, the memory must be deallocated. As we can see, there is no difference between the free and cudaFree functions, unlike the previous equivalents.<\/p>"
+  },
 ];
 
 
--- assets/js/content/code.js
+++ assets/js/content/code.js
@@ -85,47 +85,44 @@ var resizeEvent = function() {
 #define N 1&lt;&lt;20 \n\
 #define M 512 \n\
 __global__ void reverse(int *d){ \n\
-__shared__ int s[N]; \n\
-int t = threadIdx.x; \n\
-int tr = N-t-1; \n\
-s[t] = d[t]; \n\
-__syncthreads(); \n\
-d[t] = s[tr]; \n\
+\t__shared__ int s[N]; \n\
+\tint t = threadIdx.x; \n\
+\tint tr = N-t-1; \n\
+\ts[t] = d[t]; \n\
+\t__syncthreads(); \n\
+\td[t] = s[tr]; \n\
 } \n\
  \n\
 __global__ void saxpy(float a, float *x, float *y){ \n\
-int i = blockIdx.x*blockDim.x + threadIdx.x; \n\
-if (i &lt; N) y[i] = a*x[i] + y[i]; \n\
+\tint i = blockIdx.x*blockDim.x + threadIdx.x; \n\
+\tif (i &lt; N) y[i] = a*x[i] + y[i]; \n\
 } \n\
  \n\
 int main(void){ \n\
-float *x, *y, *d_x, *d_y; \n\
-x = (float*)malloc(N*sizeof(float)); \n\
-y = (float*)malloc(N*sizeof(float)); \n\
- \n\
-cudaMalloc(&d_x, N*sizeof(float)); \n\
-cudaMalloc(&d_y, N*sizeof(float)); \n\
- \n\
-for (int i = 0; i &lt; N; i++) { \n\
-x[i] = 1.0f; \n\
-y[i] = 2.0f; \n\
-} \n\
- \n\
-cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice); \n\
-cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice); \n\
- \n\
-\/\/ Perform SAXPY on 1M elements \n\
-saxpy&lt;&lt;&lt;(N+M-1)\/M, M&gt;&gt;&gt;(2.0f, d_x, d_y); \n\
-\/\/ Copy back result \n\
-cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost); \n\
-\/\/ Reverse content of x vector using parallel threads and copy back the result \n\
-reverse&lt;&lt;&lt;1, N&gt;&gt;&gt;(d_x); \n\
-cudaMemcpy(x, d_x, N*sizeof(float), cudaMemcpyDeviceToHost); \n\
- \n\
- \n\
-free(x); free(y); \n\
-cudaFree(d_x); cudaFree(d_y); \n\
-return 0; \n\
+\tfloat *x, *y, *d_x, *d_y; \n\
+\tx = (float*)malloc(N*sizeof(float)); \n\
+\ty = (float*)malloc(N*sizeof(float)); \n\
+ \t\n\
+\tcudaMalloc(&d_x, N*sizeof(float)); \n\
+\tcudaMalloc(&d_y, N*sizeof(float)); \n\
+ \t\n\
+\tfor (int i = 0; i &lt; N; i++) { \n\
+\t\tx[i] = 1.0f; \n\
+\t\ty[i] = 2.0f; \n\
+\t} \n\
+ \t\n\
+\tcudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice); \n\
+\tcudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice); \n\
+ \t\n\
+\tsaxpy&lt;&lt;&lt;(N+M-1)\/M, M&gt;&gt;&gt;(2.0f, d_x, d_y); \n\
+\tcudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost); \n\
+\treverse&lt;&lt;&lt;1, N&gt;&gt;&gt;(d_x); \n\
+\tcudaMemcpy(x, d_x, N*sizeof(float), cudaMemcpyDeviceToHost); \n\
+ \t\n\
+ \t\n\
+\tfree(x); free(y); \n\
+\tcudaFree(d_x); cudaFree(d_y); \n\
+\treturn 0; \n\
 } \
                      <\/pre> \n\
                 <\/div> \n\
